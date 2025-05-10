e7941812602482df852133df94f3c80c1eb5d9d7
LEEWOOWON <leewoon95@outlook.com>
2025-05-11 08:05:42 +0900

    VER1 03_TEST: VC 연결 및 실행 04_TEST: VC, PySide6 설치 (https://www.youtube.com/watch?v=BGJ5ZBXMkfY&t=282s) 05_TEST: QT designer 예제 VER 2 05_TEST: QT designer, 버튼, edeit 전송

---
+++ 05_Test/GuitTest.py
@@ -0,0 +1,30 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtCore import QFile
+from un_test import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.button_was_clicked)
+        self.ui.pushButton_2.clicked.connect(self.button2_was_clicked)
+        self.ui.pushButton_3.clicked.connect(self.button3_was_clicked)
+        
+    def button_was_clicked(self):
+        self.ui.textEdit.append("첫 번째 버튼이 클릭되었습니다.")
+
+    def button2_was_clicked(self):
+        self.ui.textEdit.append("두 번째 버튼이 클릭되었습니다.")
+
+    def button3_was_clicked(self):
+        self.ui.textEdit.append("세 번째 버튼이 클릭되었습니다.")
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
---
+++ 05_Test/un_test.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="un_test.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="un_test.ui" line="23"/>
+        <location filename="un_test.ui" line="32"/>
+        <location filename="un_test.ui" line="39"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
